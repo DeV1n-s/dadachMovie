de2cdfeeeec8d7652820ada0d0cdd74d539fd1ab
Aria Fathi <fresh1995boy@gmail.com>
2021-01-22 16:14:06 +0330

    Removed .nuxt folder

--- ClientApp/.nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['myMiddleware'] = require('../middleware/myMiddleware.js')
-middleware['myMiddleware'] = middleware['myMiddleware'].default || middleware['myMiddleware']
-
-export default middleware
